a7f387597b8395fbc6904371f22d5f6ad41366bd
aattoa <aattoa@proton.me>
2024-05-18 14:13:15 +0300

    Status messages
    - expand: warn when snippet body is invalid
    - complete: notify when no completion items

--- lua/simple-snippets/init.lua
+++ lua/simple-snippets/init.lua
@@ -21,9 +21,9 @@ local function set_cursor_column(column)
     vim.api.nvim_win_set_cursor(0, { vim.api.nvim_win_get_cursor(0)[1], column })
 end
 
----@type fun(filetype: string, name: string): simple-snippets.Snippet?
-local function find_snippet(filetype, name)
-    return vim.tbl_get(M.snippets, filetype, name)
+---@type fun(name: string): simple-snippets.Snippet?
+local function find_snippet(name)
+    return vim.tbl_get(M.snippets, vim.bo.filetype, name) or vim.tbl_get(M.snippets, "all", name)
 end
 
 ---@type fun(snippet: simple-snippets.Snippet): string?
@@ -35,14 +35,19 @@ local function snippet_body(snippet)
     end
 end
 
----@type fun(name: string, offset: integer): boolean
-local function expand_snippet_before_cursor(name, offset)
-    local snippet = find_snippet(vim.bo.filetype, name) or find_snippet("all", name)
-    if not snippet then return false end
-    erase_range_on_current_line(offset, name:len())
-    set_cursor_column(offset)
-    vim.snippet.expand(assert(snippet_body(snippet)))
-    return true
+---@type fun(name: string, offset: integer, snippet: simple-snippets.Snippet): boolean
+local function expand_snippet_before_cursor(name, offset, snippet)
+    local body = snippet_body(snippet)
+    if type(body) == "string" then
+        erase_range_on_current_line(offset, name:len())
+        set_cursor_column(offset)
+        vim.snippet.expand(body)
+        return true
+    else
+        local message = "nvim-simple-snippets: Attempted to expand invalid snippet: { %s = %s }"
+        vim.notify(message:format(name, vim.inspect(snippet)), vim.log.levels.WARN)
+        return false
+    end
 end
 
 ---@type fun(line: string): string?, integer
@@ -54,10 +59,10 @@ end
 ---If there is a snippet name before the cursor, expand it.
 ---@return boolean success Whether a snippet was expanded.
 M.expand = function ()
-    local line = vim.api.nvim_get_current_line()
-    local cursor = vim.api.nvim_win_get_cursor(0)
-    local name, offset = simple_word_suffix(line:sub(1, cursor[2]))
-    return name and expand_snippet_before_cursor(name, offset) or false
+    local cursor = vim.api.nvim_win_get_cursor(0)[2]
+    local name, offset = simple_word_suffix(vim.api.nvim_get_current_line():sub(1, cursor))
+    local snippet = name and find_snippet(name)
+    return snippet and expand_snippet_before_cursor(assert(name), offset, snippet) or false
 end
 
 ---If there is a snippet name before the cursor, expand it. Otherwise jump to the next snippet tabstop.
@@ -68,18 +73,22 @@ end
 ---Display available snippets in a popup-menu, and expand the selection.
 M.complete = function ()
     local snippets = vim.tbl_extend("force", M.snippets.all or {}, M.snippets[vim.bo.filetype] or {})
-    local column = vim.fn.col(".")
-    vim.fn.complete(column, vim.tbl_keys(snippets))
-    vim.api.nvim_create_autocmd("CompleteDone", {
-        callback = function ()
-            local word = vim.v.completed_item.word ---@type string?
-            if word and #word ~= 0 then
-                assert(expand_snippet_before_cursor(word, column - 1))
-            end
-        end,
-        buffer = vim.api.nvim_get_current_buf(),
-        once   = true,
-    })
+    if vim.tbl_isempty(snippets) then
+        vim.notify("nvim-simple-snippets: No snippets available", vim.log.levels.INFO)
+    else
+        local column = vim.fn.col('.')
+        vim.fn.complete(column, vim.tbl_keys(snippets))
+        vim.api.nvim_create_autocmd("CompleteDone", {
+            callback = function ()
+                local word = vim.v.completed_item.word ---@type string?
+                if word and #word ~= 0 then
+                    assert(expand_snippet_before_cursor(word, column - 1, snippets[word]))
+                end
+            end,
+            buffer = vim.api.nvim_get_current_buf(),
+            once   = true,
+        })
+    end
 end
 
 return M
