93a4447a42a8d1cc92ef28dd14ad50840c9c3cbb
aattoa <aattoa@proton.me>
2024-05-23 15:50:44 +0300

    Various improvements

--- lua/simple-snippets/init.lua
+++ lua/simple-snippets/init.lua
@@ -6,6 +6,11 @@ local M = {}
 ---@type table<string, table<string, simple-snippets.Snippet>>
 M.snippets = {}
 
+---@type fun(message: string, level: integer?)
+local function notify(message, level)
+    vim.notify("nvim-simple-snippets: " .. message, level or vim.log.levels.INFO)
+end
+
 ---@return string?
 local function treesitter_language_under_cursor()
     local line, col = unpack(vim.api.nvim_win_get_cursor(0))
@@ -19,24 +24,23 @@ local function current_filetype()
     return highlighter and treesitter_language_under_cursor() or vim.bo.filetype
 end
 
----@type fun(str: string, from: integer, to: integer): string
-local function string_erase(str, from, to)
-    return str:sub(1, from) .. str:sub(to + 1, -1)
+---@type fun(): table<string, simple-snippets.Snippet>
+local function snippets_for_current_filetype()
+    return vim.tbl_extend("keep", M.snippets[current_filetype()] or {}, M.snippets.all or {})
 end
 
----@type fun(offset: integer, length: integer): nil
-local function erase_range_on_current_line(offset, length)
-    vim.api.nvim_set_current_line(string_erase(vim.api.nvim_get_current_line(), offset, offset + length))
+---@type fun(from: integer, to: integer)
+local function erase_range_on_current_line(from, to)
+    local line = vim.api.nvim_get_current_line()
+    vim.api.nvim_set_current_line(line:sub(1, from) .. line:sub(to + 1, -1))
 end
 
----@type fun(column: integer): nil
-local function set_cursor_column(column)
-    vim.api.nvim_win_set_cursor(0, { vim.api.nvim_win_get_cursor(0)[1], column })
-end
-
----@type fun(name: string): simple-snippets.Snippet?
-local function find_snippet(name)
-    return vim.tbl_get(M.snippets, current_filetype(), name) or vim.tbl_get(M.snippets, "all", name)
+---@param word string
+local function erase_word_before_cursor(word)
+    local old_cursor = vim.fn.col('.') - 1
+    local new_cursor = old_cursor - #word
+    erase_range_on_current_line(new_cursor, old_cursor)
+    vim.api.nvim_win_set_cursor(0, { vim.fn.line('.'), new_cursor })
 end
 
 ---@type fun(snippet: simple-snippets.Snippet): string?
@@ -44,38 +48,50 @@ local function snippet_body(snippet)
     if type(snippet) == "string" then
         return snippet
     elseif type(snippet) == "function" then
-        return snippet()
-    end
-end
-
----@type fun(name: string, offset: integer, snippet: simple-snippets.Snippet): boolean
-local function expand_snippet_before_cursor(name, offset, snippet)
-    local body = snippet_body(snippet)
-    if type(body) == "string" then
-        erase_range_on_current_line(offset, name:len())
-        set_cursor_column(offset)
-        vim.snippet.expand(body)
-        return true
+        return snippet_body(snippet())
     else
-        local message = "nvim-simple-snippets: Attempted to expand invalid snippet: { %s = %s }"
-        vim.notify(message:format(name, vim.inspect(snippet)), vim.log.levels.WARN)
-        return false
+        notify("Attempted to expand invalid snippet: " .. vim.inspect(snippet), vim.log.levels.WARN)
     end
 end
 
----@type fun(line: string): string?, integer
+---@type fun(line: string): string?
 local function simple_word_suffix(line)
-    local word, prefix = line:reverse():match("^(%a+)(.*)") ---@type string?, string?
-    return word and word:reverse(), prefix and prefix:len() or 0
+    local word = line:reverse():match("^(%a+)") ---@type string?
+    return word and word:reverse()
+end
+
+---@type fun(name: string): string?
+local function find_snippet(name)
+    local snippets = snippets_for_current_filetype()
+    local snippet = snippets and snippets[name]
+    return snippet and snippet_body(snippet)
+end
+
+---@param snippets table<string, simple-snippets.Snippet>
+local function complete_snippets(snippets)
+    vim.fn.complete(vim.fn.col('.'), vim.tbl_keys(snippets))
+    vim.api.nvim_create_autocmd("CompleteDone", {
+        callback = function ()
+            local word = vim.v.completed_item.word ---@type string?
+            local body = word and #word ~= 0 and snippet_body(snippets[word])
+            if not (word and body) then return end
+            erase_word_before_cursor(word)
+            vim.snippet.expand(body)
+        end,
+        buffer = vim.api.nvim_get_current_buf(),
+        once   = true,
+    })
 end
 
 ---If there is a snippet name before the cursor, expand it.
 ---@return boolean success Whether a snippet was expanded.
 M.expand = function ()
-    local cursor = vim.api.nvim_win_get_cursor(0)[2]
-    local name, offset = simple_word_suffix(vim.api.nvim_get_current_line():sub(1, cursor))
-    local snippet = name and find_snippet(name)
-    return snippet and expand_snippet_before_cursor(assert(name), offset, snippet) or false
+    local name = simple_word_suffix(vim.api.nvim_get_current_line():sub(1, vim.fn.col('.') - 1))
+    local body = name and find_snippet(name)
+    if not name or not body then return false end
+    erase_word_before_cursor(name)
+    vim.snippet.expand(body)
+    return true
 end
 
 ---If there is a snippet name before the cursor, expand it. Otherwise jump to the next snippet tabstop.
@@ -85,22 +101,11 @@ end
 
 ---Display available snippets in a popup-menu, and expand the selection.
 M.complete = function ()
-    local snippets = vim.tbl_extend("force", M.snippets.all or {}, M.snippets[current_filetype()] or {})
+    local snippets = snippets_for_current_filetype()
     if vim.tbl_isempty(snippets) then
-        vim.notify("nvim-simple-snippets: No snippets available", vim.log.levels.INFO)
+        notify("No snippets available")
     else
-        local column = vim.fn.col('.')
-        vim.fn.complete(column, vim.tbl_keys(snippets))
-        vim.api.nvim_create_autocmd("CompleteDone", {
-            callback = function ()
-                local word = vim.v.completed_item.word ---@type string?
-                if word and #word ~= 0 then
-                    assert(expand_snippet_before_cursor(word, column - 1, snippets[word]))
-                end
-            end,
-            buffer = vim.api.nvim_get_current_buf(),
-            once   = true,
-        })
+        complete_snippets(snippets)
     end
 end
 
